804729b5bde5f132b5f311d68c3f74ce7d896edf
crisostomocharles <crisostomocharles>
2017-08-28 19:51:20 +0800

    configure db.php

--- Application/basic/config/db.php
+++ Application/basic/config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=housekeeping',
     'username' => 'root',
-    'password' => 'housekeeping',
+    'password' => '',
     'charset' => 'utf8',
 ];
